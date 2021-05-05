556e93cead7b6cd31ea195546aa8574b567ee6ac
Alison Williams <apw5011@gmail.com>
2021-05-05 15:30:02 -0400

    added seed data for ingredient class

--- meal_planner_backend/db/seeds.rb
+++ meal_planner_backend/db/seeds.rb
@@ -5,7 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Meal.destroy_all
+Ingredient.destroy_all
 
 meal1 = Meal.create(name: 'BLT Sandwich', category: 'Main', description: 'bacon, lettuce and tomato on bread')
 meal2 = Meal.create(name: 'Yogurt Parfait', category: 'Snack', description: 'nonfat yogurt, granola and blueberries')
 meal3 = Meal.create(name: 'Egg and Cheese Omelet', category: 'Breakfast', description: 'three eggs and american cheese')
+
+lettuce = Ingredient.create(name: 'iceberg lettuce', amount: '1/15 of head of lettuce', meal_id: meal1.id)
+bacon = Ingredient.create(name: 'turkey bacon', amount: '4 slices', meal_id: meal1.id)
+tomato = Ingredient.create(name: 'campari tomato', amount: '1 tomato', meal_id: meal1.id)
+
+
