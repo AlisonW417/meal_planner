80d296d6098df3191c7f83473211e48d6a73b5c3
Alison Williams <apw5011@gmail.com>
2021-05-06 16:28:44 -0400

    added additional seed data

--- meal_planner_backend/db/seeds.rb
+++ meal_planner_backend/db/seeds.rb
@@ -8,12 +8,19 @@
 Meal.destroy_all
 Ingredient.destroy_all
 
-meal1 = Meal.create(name: 'BLT Sandwich', category: 'Main', description: 'bacon, lettuce and tomato on bread')
+meal1 = Meal.create(name: 'BLT Sandwich', category: 'Lunch', description: 'bacon, lettuce and tomato on bread')
 meal2 = Meal.create(name: 'Yogurt Parfait', category: 'Snack', description: 'nonfat yogurt, granola and blueberries')
 meal3 = Meal.create(name: 'Egg and Cheese Omelet', category: 'Breakfast', description: 'three eggs and american cheese')
+meal4 = Meal.create(name: 'Pasta and Turkey Meatballs', category: 'Dinner', description: 'whole grain penne with turkey meatballs in marinara sauce')
+meal5 = Meal.create(name: 'Greek Salad', category: 'Lunch', description: 'mixed greens with cucumber, red onion, olives, tomato, and red pepper')
+meal6 = Meal.create(name: 'Strawberry Banana Smoothie', category: 'Snack', description: 'frozen strawberry and banana blended with almond milk')
+meal7 = Meal.create(name: 'Avocado Toast with Egg', category: 'Breakfast', description: 'whole grain toast topped with mashed avocado and 2 poached eggs')
+meal8 = Meal.create(name: 'Beef Burrito Bowl', category: 'Dinner', description: 'ground beef over rice and assorted veggies')
+meal9 = Meal.create(name: 'Overnight Oats', category: 'Breakfast', description: 'rolled oats, almond milk and choice of toppings')
 
 lettuce = Ingredient.create(name: 'iceberg lettuce', amount: '1/15 of head of lettuce', meal_id: meal1.id)
 bacon = Ingredient.create(name: 'turkey bacon', amount: '4 slices', meal_id: meal1.id)
 tomato = Ingredient.create(name: 'campari tomato', amount: '1 tomato', meal_id: meal1.id)
+yogurt = Ingredient.create(name: 'lowfat vanilla yogurt', amount: '5.3 ounces', meal_id: meal2.id)
 
 
