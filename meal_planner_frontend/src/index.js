128d49e88a641d0e18579047df5f51ef807398a5
Alison Williams <apw5011@gmail.com>
2021-05-06 22:39:22 -0400

    added notes for next session

--- meal_planner_frontend/src/index.js
+++ meal_planner_frontend/src/index.js
@@ -70,3 +70,5 @@ function displayMeals() {
 
 // get elements by class in order to append all meal items at once
 // remove appContainer and dailyMealPlan classes?
+// make one button for updating meals and set to limit of 7 to populate meals
+// change direction of table? 
