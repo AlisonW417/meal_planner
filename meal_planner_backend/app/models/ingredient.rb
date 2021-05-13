cd0b903ca4fd68ad286e153bfa7533477f93578d
Alison Williams <apw5011@gmail.com>
2021-05-13 11:31:52 -0400

    added basic validations

--- meal_planner_backend/app/models/ingredient.rb
+++ meal_planner_backend/app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ApplicationRecord
     belongs_to :meal
+    validates :name, presence: true
+    validates :amount, presence: true
 end
--- meal_planner_backend/app/models/meal.rb
+++ meal_planner_backend/app/models/meal.rb
@@ -1,3 +1,5 @@
 class Meal < ApplicationRecord
     has_many :ingredients
+    validates :name, presence: true
+    validates :category, presence: true
 end
