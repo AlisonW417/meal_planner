2e388762a9c17c81099733df34177d469221e56a
Alison Williams <apw5011@gmail.com>
2021-05-13 11:29:18 -0400

    html and css updates

--- meal_planner_frontend/src/formLoader.js
+++ meal_planner_frontend/src/formLoader.js
@@ -2,13 +2,15 @@ class FormLoader {
     static loadIngredientForm(currentDiv, selectedMeal) {
         let currentMeal = meals.find(meal => {return meal.name === selectedMeal});
         let ingredientForm = document.createElement('form');
-        let formHead = document.createElement('h5');
+        let formDiv = document.createElement('div');
+        let formHead = document.createElement('legend');
         let nameLabel = document.createElement('label');
         let nameInput = document.createElement('input');
         let amtLabel = document.createElement('label');
         let amtInput = document.createElement('input');
         let button = document.createElement('button');
         let hidden = document.createElement('input');
+        formDiv.setAttribute('class', 'form-group');
         hidden.setAttribute('type', 'hidden');
         hidden.setAttribute('name', 'meal');
         hidden.setAttribute('value', currentMeal.id);
@@ -17,22 +19,27 @@ class FormLoader {
         button.innerText = "Add";
         nameInput.setAttribute('type', 'text');
         nameInput.setAttribute('name', 'name');
+        nameInput.setAttribute('class', 'form-control');
         amtInput.setAttribute('type', 'text');
         amtInput.setAttribute('name', 'amount');
+        amtInput.setAttribute('class', 'form-control');
         nameLabel.setAttribute('for', 'name');
+        nameLabel.setAttribute('class', 'col-form-label mt-4');
         amtLabel.setAttribute('for', 'amount');
+        amtLabel.setAttribute('class', 'col-form-label mt-4')
         nameLabel.innerText = "Name:";
         amtLabel.innerText = "Amount:";
         formHead.innerText = "Add an ingredient";
         ingredientForm.setAttribute('id', 'new-ingredient-form');
-        ingredientForm.appendChild(formHead);
         ingredientForm.appendChild(nameLabel);
         ingredientForm.appendChild(nameInput);
         ingredientForm.appendChild(amtLabel);
         ingredientForm.appendChild(amtInput);
         ingredientForm.appendChild(hidden);
         ingredientForm.appendChild(button);
-        currentDiv.appendChild(ingredientForm);
+        formDiv.appendChild(formHead);
+        formDiv.appendChild(ingredientForm);
+        currentDiv.appendChild(formDiv);
         ingredientForm.addEventListener('submit', createNewIngredient);
     }
 
@@ -40,8 +47,8 @@ class FormLoader {
         let currentMeal = meals.find(meal => {return meal.name === selectedMeal});
         let mealPlanForm = document.createElement('form');
         let formDiv = document.createElement('div');
-        formDiv.setAttribute('class', 'form-check');
-        let formHead = document.createElement('h5');
+        formDiv.setAttribute('class', 'form-group');
+        let formHead = document.createElement('legend');
         formHead.innerText = "Select days to add this meal to your Meal Plan";
         let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
         days.forEach(day => {
@@ -62,13 +69,13 @@ class FormLoader {
         submit.setAttribute('class', 'btn btn-primary');
         submit.innerText = "Add to meal plan";
         mealPlanForm.appendChild(submit);
-        mealPlanForm.appendChild(formDiv);
+        formDiv.appendChild(formHead);
+        formDiv.appendChild(mealPlanForm);
         mealPlanForm.addEventListener('submit', (event) => {
             event.preventDefault();
             let div = event.target.parentElement.id;
             renderMealPlan(currentMeal, mealPlanForm);
         });
-        currentDiv.appendChild(formHead);
-        currentDiv.appendChild(mealPlanForm);
+        currentDiv.appendChild(formDiv);
     }
 }
