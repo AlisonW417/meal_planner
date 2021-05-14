5c182fce7460e6d06b6d73f3f76182f235770837
Alison Williams <apw5011@gmail.com>
2021-05-14 11:09:00 -0400

    refactor index to remove fetch requests

--- meal_planner_frontend/src/index.js
+++ meal_planner_frontend/src/index.js
@@ -25,81 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
     snackSelect.addEventListener('click', renderMealInfo);
 })
 
-// READ MEALS
-// function getMeals() {
-//     fetch(`${url}/meals`)
-//     .then(resp => resp.json())
-//     .then(data => {
-//         data.forEach(meal => {
-//             new Meal(meal.name, meal.category, meal.ingredients);
-//             meals.push(meal);  
-//         })
-//     renderMeals();
-//     })
-// }
-
-// CREATE MEAL
-// function createNewMeal(event){
-//     event.preventDefault();
-//     fetch(`${url}/meals`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-type': 'application/json',
-//             'Accept': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             name: event.target.name.value,
-//             category: event.target.category.value,
-//         })
-//     })
-//     .then(resp => resp.json())
-//     .then(data => {
-//         console.log(data);
-//         if (data.message) {
-//             alert(data.message)
-//         } else {
-//             new Meal(data.name, data.category, data.ingredients);
-//             meals.push(data);
-//             let form = document.getElementById('new-meal-form');
-//             form.reset();
-//             renderMeals();
-//         }
-//     })
-// }
-
-// CREATE INGREDIENT AND RENDER TO DOM
-// function createNewIngredient(event) {
-//     event.preventDefault();
-//     fetch(`${url}/ingredients`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-type': 'application/json',
-//             'Accept': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             name: event.target.name.value,
-//             amount: event.target.amount.value,
-//             meal_id: event.target.meal.value
-//         })
-//     })
-//     .then(resp => resp.json())
-//     .then(data => {
-//         console.log(data);
-//         if (data.message) {
-//             alert(data.message)
-//         } else {
-//             meal = meals.find(meal => {return meal.id === data.meal_id});
-//             meal.ingredients.push(data);
-//             let ingredient = document.createElement('li');
-//             ingredient.innerText = `${data.name} - ${data.amount}`;
-//             let list = document.getElementById(`${meal.category.toLowerCase()}-ingredient-list`);
-//             list.appendChild(ingredient);
-//             let form = document.getElementById('new-ingredient-form');
-//             form.reset();
-//         }
-//     })
-// }
-
 // RENDER MEALS TO DOM
 function renderMeals() {
     const bfastSelect = document.getElementById('selected-breakfast');
@@ -153,6 +78,7 @@ function renderMealInfo(event){
     FormLoader.loadMealPlanForm(mealInfoDiv, selectedMeal);
 }
 
+// RENDER Meal Plan Menus to DOM
 function renderMealPlan(currentMeal, form) {
     let days = [];
     let checkboxes = document.getElementsByClassName('form-check-input');
@@ -180,6 +106,7 @@ function renderMealPlan(currentMeal, form) {
     })
 }
 
+// REMOVE Meal from menu
 function removeMealFromPlan(event){
     event.target.parentElement.remove();
 }
